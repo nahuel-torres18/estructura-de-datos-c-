ecec991d3c9c6173a87a936258fbe46ebdd26cb0
Nahuel <nahueltorres520@gmail.com>
2024-07-26 15:35:02 -0300

    primer programa

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
-    
+    int n1,n2;
+    map<int,int> pelotas;
+    cin >> n1 >> n2;
+    for (int i = 0; i < n1; i++) {
+        int num;
+        cin >> num;
+        pelotas[num]++;
+    }
+    for (int i = 1; i <= n2; i++) {
+        cout << i << ": " << pelotas[i] << endl;
+    }
 }
